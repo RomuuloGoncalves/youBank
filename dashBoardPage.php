03596f966a5b94d99136e8911f5daa4b7862c01a
Rômulo Gonçalves <romulogoncalves13@gmail.com>
2022-10-11 19:30:02 -0300

    #

--- dashBoardPage.php
+++ dashBoardPage.php
@@ -9,12 +9,12 @@ $pessoa = $pessoa->fetch(PDO::FETCH_OBJ);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>DashBoard</title>
+        <title></title>
         <link rel="stylesheet" href="./css/style.css">
         <link rel="stylesheet" href="./css/dashBoard.css">
 </head>
